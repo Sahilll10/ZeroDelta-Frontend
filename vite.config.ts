db23b29fedbde85144faf0c269d78a121183f00f
Learning <sahilgreat321@gmail.com>
2026-05-20 23:14:12 +0530

    Apply failsafe build config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,19 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  // Force the build output to be clear and simple
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      // Point explicitly to the root index.html
+      input: {
+        app: './index.html',
+      },
+    },
+  },
   resolve: {
     alias: {
+      // Use absolute pathing for the @ alias
       "@": path.resolve(__dirname, "./src"),
     },
   },
